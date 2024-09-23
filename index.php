8f538585f865ca7cda4a131c40c2ea227bb44da1
TrDungxx <Hovercard27@gmail.com>
2024-09-23 15:55:18 +0700

    1

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+$pageTitle = "Trang chủ - Nobel Prize";
+
+
+include 'header.php';
+?>
+
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+
+    <!-- Thêm Bootstrap CSS từ CDN -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- File CSS tùy chỉnh của bạn -->
+    <link rel="stylesheet" href="indexstyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+</head>
+<body>
+    <div class="background-div">
+    <a href="#" class="link-overlay">THE NOBEL PRIZE ANNOUNCEMENTS 7-14 OCTOBER2024</a>
+    </div>
+    <div class="divider"></div>
+    <div><p class="text">2024 Nobel Prize Concert</p></div>
+    <div class="divider2"></div>
+    
+</body>
+</html>
+
+
+
